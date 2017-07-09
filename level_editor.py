d072141d72b40349b36166bc1cad9e364082094a
KnifeyMoloko <maciej.a.cisowski@gmail.com>
2017-07-09 23:49:42 +0200

    Commit nr 25 - level-editor branch - browser function ctd.

--- level_editor.py
+++ level_editor.py
@@ -22,7 +22,7 @@ class Listbox():
     def __init__(self, master):
         self.listbox = Tkinter.Listbox(master, selectmode=Tkinter.BROWSE)
         self.listed = helpers.folder_browser()
-        self.current = None
+        self.current = os.getcwd() + "/Resources/"
         self.entries()
         self.listbox.grid()
         self.poll()
@@ -34,20 +34,42 @@ class Listbox():
 
     def poll(self):
         now = self.listbox.curselection()
-        if now != self.current:
-            self.list_has_changed(now)
-            self.current = now
+
+        if now != self.current and now != ():
+            if os.path.isdir(self.current+self.listbox.get(now[0])):
+                self.current = self.current + self.listbox.get(now[0]) + "/"
+                self.listed = helpers.folder_browser(self.current)
+                self.entries()
+            else:
+                filename = self.listbox.get(now[0])
+                self.current = self.current + filename
+                self.instantiate_canvas(self.current, filename)
+                return 0
         self.listbox.after(250, self.poll)
 
 
-    def list_has_changed(self, selection):
-        print "selection is ", selection
-        if len(selection) != 0:
-            new_path = os.getcwd() + "/Resources/" + self.listbox.get(selection[0])
-            print new_path
-            #TODO use os.path.isfile here to make branch the flow
-            self.listed = helpers.folder_browser(new_path)
-            self.entries()
+    def instantiate_canvas(self, sel_path, file):
+        print "selection is ", sel_path
+        print "Instantiate ", file,  "please"
+        trunc_path = sel_path[0:len(sel_path)-len(file)]
+        print "Truncated path is: " + trunc_path
+
+        #TODO make a decision button: either pick bg or continue browsing
+
+        yes_button = Tkinter.Button(mainframe,
+                                    text="Pick this file?" + str(self.current),
+                                    command=fill_background)
+        no_button = Tkinter.Button(mainframe,
+                                   text="Continue browsing",
+                                   command=self.poll)
+        #TODO use the trunc_path to browse from the one-up level
+        yes_button.grid()
+        no_button.grid()
+
+
+def fill_background():
+    print "Filling background"
+
 
 def create_listbox(frame=mainframe):
     lbox = Listbox(frame)
